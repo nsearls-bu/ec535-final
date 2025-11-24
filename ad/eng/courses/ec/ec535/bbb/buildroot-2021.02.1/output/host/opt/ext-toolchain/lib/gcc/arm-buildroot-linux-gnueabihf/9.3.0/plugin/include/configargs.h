1133e82e7287015bd46a7f914b3fe250c5802bdc
ned <nsearls@bu.edu>
2025-11-23 22:46:49 -0500

    Removed ad

--- ad/eng/courses/ec/ec535/bbb/buildroot-2021.02.1/output/host/opt/ext-toolchain/lib/gcc/arm-buildroot-linux-gnueabihf/9.3.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "./configure --prefix=/opt/armv7-eabihf--glibc--stable-2020.08-1 --sysconfdir=/opt/armv7-eabihf--glibc--stable-2020.08-1/etc --enable-static --target=arm-buildroot-linux-gnueabihf --with-sysroot=/opt/armv7-eabihf--glibc--stable-2020.08-1/arm-buildroot-linux-gnueabihf/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/opt/armv7-eabihf--glibc--stable-2020.08-1 --with-mpc=/opt/armv7-eabihf--glibc--stable-2020.08-1 --with-mpfr=/opt/armv7-eabihf--glibc--stable-2020.08-1 --with-pkgversion='Buildroot 2020.08-14-ge5a2a90' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libquadmath --disable-libquadmath-support --enable-tls --enable-threads --without-isl --without-cloog --with-abi=aapcs-linux --with-cpu=cortex-a9 --with-fpu=vfpv3-d16 --with-float=hard --with-mode=arm --enable-languages=c,c++,fortran --with-build-time-tools=/opt/armv7-eabihf--glibc--stable-2020.08-1/arm-buildroot-linux-gnueabihf/bin --enable-shared --disable-libgomp";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a9" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "vfpv3-d16" }, { "tls", "gnu" } };
